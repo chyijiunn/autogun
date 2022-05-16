2d22ec3e2445fc07986fca3ce20df36200a9f9d1
chyijiunn <chyijiunn@gmail.com>
2022-05-16 11:41:54 +0800

    Update 01_motor_mg995.py

--- 01_motor_mg995.py
+++ 01_motor_mg995.py
@@ -3,12 +3,12 @@ from time import sleep
 servo = PWM(Pin(26))     # the Pico PWM pin
 servo.freq(50)
 print(servo.duty_u16())
-'''
-servo.duty_u16(7000)   
-sleep(0.75)
-servo.duty_u16(5000)   
+
+servo.duty_u16(1000)   
+sleep(0.5)
+for i in range(200):
+    servo.duty_u16(40*i+1000)
+    sleep(0.1)
+servo.duty_u16(2000)   
 sleep(1)
-servo.duty_u16(4500)  
-sleep(0.1)      
 servo.deinit()               
-'''
